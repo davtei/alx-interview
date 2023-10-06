4c63ab7b8e0b3bfeb57d71e958477734514b8eff
David Tei <daveteidt@gmail.com>
2023-10-06 00:39:55 +0000

    add lockboxes method

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened """
+    if not boxes:
+        return False
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for newKey in boxes[key]:
+            if newKey not in keys and newKey < len(boxes):
+                keys.append(newKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
